a46b540f65541abc8f565d3306d5c1296b55d7d7
Opal <guerre5lily@gmail.com>
2021-11-03 23:59:41 -0700

    added drag and drop across columns

--- assets/js/script.js
+++ assets/js/script.js
@@ -178,3 +178,52 @@ $(".list-group").on("blur", "input[type='text']", function() {
   $(this).replaceWith(taskSpan)
 
 })
+
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this)
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this)
+  },
+  over: function(event) {
+    console.log("over", event.target)    
+  },
+  out: function(event) {
+    console.log("out", event.target)
+  },
+  update: function(event) {
+    var tempArr = []
+
+    $(this).children().each(function () {
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim()
+
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim()
+
+      tempArr.push({
+        text: text,
+        date: date
+      })
+
+      console.log(tempArr)
+    })
+    //trim down list's id to match object propert
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "")
+    //update array on tasks object and save
+    tasks[arrName] = tempArr
+    saveTasks()
+  }
+
+})
